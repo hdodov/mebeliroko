ede4104d694c327da7f922413bb9c753048553d0
Hristiyan Dodov <h.dodov@gmail.com>
2018-06-14 17:32:48 +0300

    Add duplicate gallery images toggle

--- site/controllers/gallery.php
+++ site/controllers/gallery.php
@@ -3,10 +3,23 @@
 return function ($site, $pages, $page) {
     $allImages = array();
 
+    $imageNames = array();
+    $hideDuplicates = ($page->hide_duplicates() == 'true');
+
     foreach ($site->index()->filterBy('template', 'project') as $project) {
         $images = $project->images()->sortBy('sort', 'asc');
 
         foreach ($images as $image) {
+            if ($hideDuplicates) {
+                $name = $image->name();
+
+                if (array_search($name, $imageNames) === false) {
+                    array_push($imageNames, $name);
+                } else {
+                    continue;
+                }
+            }
+
             array_push($allImages, $image);
         }
     }
